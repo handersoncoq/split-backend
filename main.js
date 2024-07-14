1e79157afc569f321289ae8edffa0dc06d8157a0
Handerson Coq <coqhandersonth@gmail.com>
2024-07-13 20:32:52 -0400

    updated comments

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';  // Make sure to import 'path'
-import { fileURLToPath } from 'url';
-import { downloadYoutubeAudio } from './controllers/ProcessController.js';
-import { processYoutubeUrl } from './services/ProcessService.js';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { downloadYoutubeAudio } from "./controllers/ProcessController.js";
+import { processYoutubeUrl } from "./services/ProcessService.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,24 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-process.on('uncaughtException', (error) => {
-  console.error('There was an uncaught error', error);
+process.on("uncaughtException", (error) => {
+  console.error("There was an uncaught error", error);
   process.exit(1);
 });
-
 
 const PORT = process.env.PORT || 4000;
 
 // Serve static files
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Routes
-app.get('/download', downloadYoutubeAudio);
-app.get('/process', processYoutubeUrl);
-
+app.get("/download", downloadYoutubeAudio);
+app.get("/process", processYoutubeUrl);
 
 // Start
 app.listen(PORT, () => {
-  console.log('\x1b[92m%s\x1b[0m', `Server running on port ${PORT}`);
+  console.log("\x1b[92m%s\x1b[0m", `Server running on port ${PORT}`);
 });
-
